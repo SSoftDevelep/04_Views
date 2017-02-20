fb36eac606dddb67754f26e61f7d044b44cea044
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-20 16:21:53 +0300

    Views,layout kullanimi

---
+++ 04_Views/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult homepage()
+        {
+            return View();
+        }
+        //hakkimizda
+        public ActionResult about()
+        {
+            return View();
+        }
+        //iletisim
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
